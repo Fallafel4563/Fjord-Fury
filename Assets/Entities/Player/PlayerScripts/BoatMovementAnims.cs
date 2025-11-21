b6dbb1a5faba6836ed29624945c0b9a54e6d1cfc
Treikeh <73736974+Treikeh@users.noreply.github.com>
2025-11-21 19:18:13 +0100

    Minor boat animation script improvements

--- Assets/Entities/Player/PlayerScripts/BoatMovementAnims.cs
+++ Assets/Entities/Player/PlayerScripts/BoatMovementAnims.cs
@@ -5,11 +5,11 @@ public class BoatMovementAnims : MonoBehaviour
     [HideInInspector] public PlayerMovement playerMovement;
 
     [Header("General")]
-    public float lerpSpeed = 5f;
+    public float lerpSpeed = 1f;
 
 
     [Header("Grounded")]
-    public float groundTilt = 1f;
+    public float groundTilt = 5f;
 
 
     [Header("Airborne")]
@@ -27,19 +27,21 @@ public class BoatMovementAnims : MonoBehaviour
 
     private void GroundedAnim()
     {
-        // Set scale
+        // Scale
         transform.localScale = Vector3.one;
 
-        // Set rotation
-        Vector3 newRotation = transform.localEulerAngles;
-        newRotation.x = -playerMovement.ySpeed * 2f;
 
+        // Rotation
+        Vector3 newRotation = transform.localEulerAngles;
+        // Pitch
+        newRotation.x = 0f;
+        // Yaw
         float yFrom = newRotation.y;
         float yTo = playerMovement.steerInput * playerMovement.steerSpeed * 2f + playerMovement.dashTime * playerMovement.dashDirection * 200f * groundTilt;
         newRotation.y = Mathf.LerpAngle(yFrom, yTo, Time.deltaTime * 5f);
-
-        newRotation.z = (playerMovement.isGrounded == false && playerMovement.isDashing) ? newRotation.z + 400f * playerMovement.dashDirection * Time.deltaTime : Mathf.LerpAngle(newRotation.z, -playerMovement.steerInput * playerMovement.steerSpeed, Time.deltaTime * 5f);
-
+        // Roll
+        newRotation.z = Mathf.LerpAngle(newRotation.z, -playerMovement.steerInput * playerMovement.steerSpeed, Time.deltaTime * 5f);
+        // Apply rotation
         transform.localEulerAngles = newRotation;
     }
 
@@ -47,22 +49,24 @@ public class BoatMovementAnims : MonoBehaviour
 
     private void AirborneAnim()
     {
-        // Set scale
+        // Scale
         float horizontalScale = Mathf.Clamp(playerMovement.timeSinceJump * playerMovement.timeSinceJump * 1.5f, 0.5f, 1f);
         float verticalScale = Mathf.Clamp(1 - playerMovement.timeSinceJump * playerMovement.timeSinceJump, Mathf.Max(playerMovement.timeSinceJump, 1f), 2f);
         transform.localScale = new(horizontalScale, verticalScale, horizontalScale);
 
-        // Set rotation
-        // Set rotation
-        Vector3 newRotation = transform.localEulerAngles;
-        newRotation.x = -playerMovement.ySpeed * 2f;
 
+        // Rotation
+        Vector3 newRotation = transform.localEulerAngles;
+        // Pitch
+        newRotation.x = -playerMovement.ySpeed * 2f;
+        newRotation.x = Mathf.Clamp(newRotation.x, -89f, 89f);
+        // Yaw
         float yFrom = newRotation.y;
         float yTo = playerMovement.steerInput * playerMovement.steerSpeed * 2f + playerMovement.dashTime * playerMovement.dashDirection * 200f * airTilt;
         newRotation.y = Mathf.LerpAngle(yFrom, yTo, Time.deltaTime * 5f);
-
-        newRotation.z = (playerMovement.isGrounded == false && playerMovement.isDashing) ? newRotation.z + 400f * playerMovement.dashDirection * Time.deltaTime : Mathf.LerpAngle(newRotation.z, -playerMovement.steerInput * playerMovement.steerSpeed, Time.deltaTime * 5f);
-
+        // Roll
+        newRotation.z = playerMovement.isDashing ? newRotation.z + 400f * playerMovement.dashDirection * Time.deltaTime : Mathf.LerpAngle(newRotation.z, -playerMovement.steerInput * playerMovement.steerSpeed, Time.deltaTime * 5f);
+        // Apply rotation
         transform.localEulerAngles = newRotation;
     }
 }
