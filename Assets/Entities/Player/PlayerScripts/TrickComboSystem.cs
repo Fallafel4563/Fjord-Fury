35060416aa0de1733e6f56c5994a96e9cd6ff7c8
Treikeh <73736974+Treikeh@users.noreply.github.com>
2025-11-24 11:35:48 +0100

    Started on the trick combo system

---
+++ Assets/Entities/Player/PlayerScripts/TrickComboSystem.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class TrickComboSystem : MonoBehaviour
+{
+    [HideInInspector] public PlayerMovement playerMovement;
+
+    public int combo = 0;
+    public int trickScore = 0;
+}
