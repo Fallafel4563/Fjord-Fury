ebb05c644eff3fc0108df7126f616fb9de5d228b
Fallafel4563 <70334545+Fallafel4563@users.noreply.github.com>
2025-12-11 18:10:05 +0100

    la til slipstream og placements

--- Assets/Entities/Player/PlacementText.cs
+++ Assets/Entities/Player/PlacementText.cs
@@ -4,6 +4,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Splines;
+using UnityEngine.UI;
 
 public class PlacementText : MonoBehaviour
 {
@@ -14,45 +15,56 @@ public class PlacementText : MonoBehaviour
     //public SplineTrackDistance distanceSpline;
     public List<string> placementSuffixes = new List<string>();
     public SplineTrackDistance splineDistance;
-  
+    public List<Sprite> placementImages = new List<Sprite>();
+    private Image placementImage;
+
     private int playerIndex;
     public float distanceAlongTrack;
     public static Dictionary<int, float> DistancesAlongSpline = new Dictionary<int, float>();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {  
+    {
         splineDistance = GetComponent<SplineTrackDistance>();
         placementText = playerControllerRef.playerHud.placementText;
         //distanceSpline = GameObject.FindWithTag("Distance Tracker").GetComponent<SplineContainer>(); 
         playerIndex = playerInput.user.index;
+        placementImage = playerControllerRef.playerHud.placementImage;
     }
 
     // Update is called once per frame
     void Update()
     {
         distanceAlongTrack = splineDistance.distanceAlongSpline;
-        DistancesAlongSpline[ playerInput.playerIndex] = distanceAlongTrack;
+        DistancesAlongSpline[playerInput.playerIndex] = distanceAlongTrack;
 
-        //Debug.Log(DistancesAlongSpline.Values.ToList());
+        Debug.Log(DistancesAlongSpline.Values.ToList());
 
         List<float> valuesList = DistancesAlongSpline.Values.ToList();
         valuesList.Sort();
         valuesList.Reverse();
 
-       int Index = valuesList.IndexOf(splineDistance.distanceAlongSpline);
-       //Debug.Log(Index + 1 + placementSuffixes[Index] + " playerIndex " + playerInput.playerIndex);
+        int Index = valuesList.IndexOf(splineDistance.distanceAlongSpline);
+        Debug.Log(Index + 1 + placementSuffixes[Index] + " playerIndex " + playerInput.playerIndex);
+        //Debug.Log(Index + 1 + placementImages[Index] + " playerIndex " + playerInput.playerIndex);
 
-      // float placement = Index;
 
-       placementText.text =Index + 1 + placementSuffixes[Index];
-       string firstPlace = placementSuffixes[0]; 
-       string secondPlace = placementSuffixes[1]; 
-       string thirdPlace = placementSuffixes[2]; 
-       string fourthPlace = placementSuffixes[3]; 
 
-       //DistancesAlongSpline[ PlayerInput.playerIndex] = distanceAlongTrack;
-       //
-       //placementText = placement + 1 + placementSuffixes[placement];
+        placementText.text = Index + 1 + placementSuffixes[Index];
+        placementImage.sprite = placementImages[Index];
+        playerControllerRef.playerHud.SetFirstPlayerShine(Index + 1);
+        /*
+        string firstPlace = placementSuffixes[0];
+        string secondPlace = placementSuffixes[1];
+        string thirdPlace = placementSuffixes[2];
+        string fourthPlace = placementSuffixes[3];
+
+        Sprite firstPlaceImage = placementImages[0];
+        Sprite secondPlaceImage = placementImages[1];
+        Sprite thirdPlaceImage = placementImages[2];
+        Sprite fourthPlaceImage = placementImages[3];
+        */
+
     }
 
 }
