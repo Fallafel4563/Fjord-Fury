368a0c46ae53e779344cee0900d1904d9c92c6bc
StianGunnarsen <stianhgun@hotmail.com>
2025-12-12 10:13:54 +0100

    sliptrail and placement

--- Assets/Entities/Player/PlacementText.cs
+++ Assets/Entities/Player/PlacementText.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Splines;
 using UnityEngine.UI;
 
 public class PlacementText : MonoBehaviour
@@ -27,7 +26,7 @@ public class PlacementText : MonoBehaviour
     {  
         splineDistance = GetComponent<SplineTrackDistance>();
         placementText = playerControllerRef.playerHud.placementText;
-        //distanceSpline = GameObject.FindWithTag("Distance Tracker").GetComponent<SplineContainer>(); 
+
         playerIndex = playerInput.user.index;
         placementImage = playerControllerRef.playerHud.placementImage;
     }
@@ -45,24 +44,10 @@ public class PlacementText : MonoBehaviour
         valuesList.Reverse();
 
        int Index = valuesList.IndexOf(splineDistance.distanceAlongSpline);
-       Debug.Log(Index + 1 + placementSuffixes[Index] + " playerIndex " + playerInput.playerIndex);
-        //Debug.Log(Index + 1 + placementImages[Index] + " playerIndex " + playerInput.playerIndex);
-
       
-
        placementText.text =Index + 1 + placementSuffixes[Index];
        placementImage.sprite = placementImages[Index];
        playerControllerRef.playerHud.SetFirstPlayerShine(Index + 1);    
-
-       string firstPlace = placementSuffixes[0]; 
-       string secondPlace = placementSuffixes[1]; 
-       string thirdPlace = placementSuffixes[2]; 
-       string fourthPlace = placementSuffixes[3]; 
-
-       Sprite firstPlaceImage = placementImages[0];
-       Sprite secondPlaceImage = placementImages[1];
-       Sprite thirdPlaceImage = placementImages[2];
-       Sprite fourthPlaceImage = placementImages[3];
 
       
     }
