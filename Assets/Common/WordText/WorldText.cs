5007154b9d4d8677391d7bf4f94bf95b03417cb4
Treikeh <73736974+Treikeh@users.noreply.github.com>
2025-12-15 09:45:22 +0100

    Github messed up and the changes i made on friday weren't properly added to production. So here they are again.

--- Assets/Common/WordText/WorldText.cs
+++ Assets/Common/WordText/WorldText.cs
@@ -4,11 +4,29 @@ using UnityEngine;
 public class WorldText : MonoBehaviour
 {
     public TMP_Text textAsset;
+    public TMP_ColorGradient shroomColors;
+    public TMP_ColorGradient tornadoColors;
+    public TMP_ColorGradient ramColors;
 
 
-    public void SetUpText(string text, Color color)
+    public void SetUpText(string text, Color color, int trick)
     {
         textAsset.text = text;
-        textAsset.color = Color.white;
+
+        switch (trick)
+        {
+            case 0:
+                textAsset.colorGradientPreset = shroomColors;
+                break;
+            case 1:
+                textAsset.colorGradientPreset = tornadoColors;
+                break;
+            case 2:
+                textAsset.colorGradientPreset = ramColors;
+                break;
+            default:
+                textAsset.color = color;
+                break;
+        }
     }
 }
