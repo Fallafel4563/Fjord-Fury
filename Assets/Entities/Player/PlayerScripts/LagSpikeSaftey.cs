39244b0d657d5c0078624f149081f9f182956a8c
Treikeh <73736974+Treikeh@users.noreply.github.com>
2025-12-15 14:52:56 +0100

    Updated lag spike again

--- Assets/Entities/Player/PlayerScripts/LagSpikeSaftey.cs
+++ Assets/Entities/Player/PlayerScripts/LagSpikeSaftey.cs
@@ -16,7 +16,7 @@ public class LagSpikeSaftey : MonoBehaviour
 
     void Update()
     {
-        if (Time.deltaTime > 0.1f && playerMovement.airVelocity.magnitude > 100f)
+        if (Time.deltaTime > 0.1f && playerMovement.airVelocity.magnitude > 50f)
         {
             Vector3 rayDirection = oldPosition - (transform.position + transform.up);
             RaycastHit raycastHit;
