4e970f509e4d468864d800d4c4b8e40059077d11
ChunkyWorm <sonjaohman1310@gmail.com>
2025-12-15 17:07:18 +0100

    Go back on escape

--- Assets/GUI/Menus/Scripts/MenuManager.cs
+++ Assets/GUI/Menus/Scripts/MenuManager.cs
@@ -1,80 +1,32 @@
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class MenuManager : MonoBehaviour
 {
-    private Stack<GameObject> panelHistory = new Stack<GameObject>();
-    private GameObject currentPanel;
-
-    [Header("Root Panel")]
-    public GameObject mainMenuPanel;
-
-    void Start()
-    {
-        if (mainMenuPanel == null)
-        {
-            Debug.LogError("Main Menu Panel not assigned!");
-            return;
-        }
-
-//        SetAllPanelsInactive();
-//        currentPanel = mainMenuPanel;
-//        currentPanel.SetActive(true);
-    }
+    [SerializeField] GameObject startMenu;
+    [SerializeField] GameObject options;
+    [SerializeField] GameObject credits;
 
     void Update()
     {
-        if (Gamepad.current != null && Gamepad.current.bButton.wasPressedThisFrame)
-        {
-            GoBack();
-        }
-
-        // Keyboard back (Escape)
         if (Keyboard.current != null && Keyboard.current.escapeKey.wasPressedThisFrame)
         {
-            GoBack();
+            HandleBack();
         }
-    }
-
-
-    public void OpenPanel(GameObject panelToOpen)
-    {
-        if (panelToOpen == null || panelToOpen == currentPanel)
-            return;
-
-        if (currentPanel != null)
+        if (Gamepad.current != null && Gamepad.current.bButton.wasPressedThisFrame)
         {
-            panelHistory.Push(currentPanel);
-            currentPanel.SetActive(false);
+            HandleBack();
         }
-
-        currentPanel = panelToOpen;
-        currentPanel.SetActive(true);
     }
 
-
-    public void GoBack()
+    private void HandleBack()
     {
-        if (panelHistory.Count == 0)
+        if (options.activeSelf || credits.activeSelf)
         {
-            Debug.Log("No previous panel in history");
-            return;
+            options.SetActive(false);
+            credits.SetActive(false);
+            startMenu.SetActive(true);
         }
 
-        currentPanel.SetActive(false);
-        currentPanel = panelHistory.Pop();
-        currentPanel.SetActive(true);
     }
-
-    /// <summary>
-    /// Disable all panels under this canvas
-    /// </summary>
-//    private void SetAllPanelsInactive()
-//    {
-//        foreach (Transform child in mainMenuPanel.transform.parent)
-//        {
-//            child.gameObject.SetActive(false);
-//        }
-//    }
 }
