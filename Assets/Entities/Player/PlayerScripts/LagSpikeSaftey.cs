4f02015f6e3e66c22ac9742284ed5b5ddb7de033
Treikeh <73736974+Treikeh@users.noreply.github.com>
2025-12-15 15:34:50 +0100

    a

--- Assets/Entities/Player/PlayerScripts/LagSpikeSaftey.cs
+++ Assets/Entities/Player/PlayerScripts/LagSpikeSaftey.cs
@@ -16,7 +16,7 @@ public class LagSpikeSaftey : MonoBehaviour
 
     void Update()
     {
-        if (Time.deltaTime > 0.1f && playerMovement.airVelocity.magnitude > 50f)
+        if (Time.deltaTime > 0.1f && playerMovement.airVelocity.magnitude > 10f)
         {
             Vector3 rayDirection = oldPosition - (transform.position + transform.up);
             RaycastHit raycastHit;
@@ -26,8 +26,7 @@ public class LagSpikeSaftey : MonoBehaviour
                 playerMovement.airVelocity = Vector3.zero;
                 transform.position = raycastHit.point;
             }
-
-            oldPosition = transform.position + transform.up;
         }
+        oldPosition = transform.position + transform.up;
     }
 }
