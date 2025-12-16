59775592d33c8bc7dfb93501c39c0c24b14aa695
Fallafel4563 <70334545+Fallafel4563@users.noreply.github.com>
2025-12-16 09:13:25 +0100

    Squashed commit of the following:
    commit 833cc088ed6041913b3053a200721c939b710ca4
    Author: Fallafel4563 <70334545+Fallafel4563@users.noreply.github.com>
    Date:   Tue Dec 16 09:12:43 2025 +0100
    
        fmod ceaser
    
    commit 8cf9804dff0802779f8a41df434f37e0a8cd493a
    Merge: 451899f 0af2ea4
    Author: Fallafel4563 <70334545+Fallafel4563@users.noreply.github.com>
    Date:   Tue Dec 16 09:06:52 2025 +0100
    
        Merge branch 'Build1512LastJorgen' into build-merge-test
    
    commit 0af2ea46e51e062dfe8d51e6e42db84f792490b2
    Author: Fallafel4563 <70334545+Fallafel4563@users.noreply.github.com>
    Date:   Mon Dec 15 20:20:39 2025 +0100
    
        build done
    
    commit a2289fe06cb470ab5b5c9792cd649f1bd084f86f
    Merge: f1aa52c 58719f4
    Author: Fallafel4563 <70334545+Fallafel4563@users.noreply.github.com>
    Date:   Mon Dec 15 19:32:24 2025 +0100
    
        Merge branch 'production' into Build1512LastJorgen
    
    commit f1aa52cef5f21df03077d8bf42107750a6e19512
    Merge: f57fd40 a8fdd57
    Author: Fallafel4563 <70334545+Fallafel4563@users.noreply.github.com>
    Date:   Mon Dec 15 19:22:48 2025 +0100
    
        Merge branch 'production' into Build1512LastJorgen
    
    commit f57fd408794d02438f80dff4474aefe945adc8a1
    Merge: ec632f4 13b8920
    Author: Fallafel4563 <70334545+Fallafel4563@users.noreply.github.com>
    Date:   Mon Dec 15 19:21:50 2025 +0100
    
        Merge branch 'production' into Build1512LastJorgen
    
    commit ec632f4496168875dd4e0f9692efb35444ed4c3b
    Author: Fallafel4563 <70334545+Fallafel4563@users.noreply.github.com>
    Date:   Mon Dec 15 19:21:44 2025 +0100
    
        added in music and water tags on level 1 and end triggers and fixed ram and more
    
    commit 09bed73b6149cbbcdd32fc745b983d84af9504a1
    Author: Fallafel4563 <70334545+Fallafel4563@users.noreply.github.com>
    Date:   Mon Dec 15 18:13:08 2025 +0100
    
        tried to merge in the new character select

--- Assets/GUI/Menus/Scripts/MenuStart.cs
+++ Assets/GUI/Menus/Scripts/MenuStart.cs
@@ -13,9 +13,13 @@ public class MenuStart : MonoBehaviour
     [SerializeField] GameObject optionsMenu;     
     [SerializeField] GameObject credits;
     [SerializeField] GameObject riverRally;
-    [SerializeField] GameObject valHalla;    
+    [SerializeField] GameObject valHalla;
     #endregion
 
+    private void Start()
+    {
+        Time.timeScale = 1.0f;
+    }
 
     #region Play
 
@@ -31,11 +35,21 @@ public class MenuStart : MonoBehaviour
     {
         SceneManager.LoadScene("CharacterSelectExampleScene");
     }
+
+    public void onStartValhallaButton()
+    {
+        SceneManager.LoadScene("ValhallaCharacterSelect");
+    }
+
+    public void loadLevelSelect()
+    {
+        SceneManager.LoadScene("LevelSelect");
+    }
     #endregion
 
 
     #region Options
-    
+
     // Takes you to options screen
     public void OnOptionsButton()
     {   
