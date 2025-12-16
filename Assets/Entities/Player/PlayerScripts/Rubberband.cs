9fae893d2c4ce40d0d286f4b6bd6e0c6d4c8f81f
Treikeh <73736974+Treikeh@users.noreply.github.com>
2025-12-16 14:24:24 +0100

    Implemented rubberband system

---
+++ Assets/Entities/Player/PlayerScripts/Rubberband.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Ruber : MonoBehaviour
+{
+    public ForwardSpeedMultiplier forwardSpeedMultiplier;
+    public PlacementText placementText;
+
+    public AnimationCurve rubberbandCurve;
+
+    public float rubberbandBoost { get; private set; }
+    public float furthestPlayerDistance { get; private set; }
+    public float distance { get; private set; }
+
+
+    private void FixedUpdate()
+    {
+        distance = placementText.distanceAlongTrack;
+        furthestPlayerDistance = placementText.valuesList[0];
+        rubberbandBoost = rubberbandCurve.Evaluate(furthestPlayerDistance - distance);
+
+        forwardSpeedMultiplier.SetForwardSpeedMultiplier("Rubberband boost", 1f + rubberbandBoost);
+        Debug.LogFormat("Rubberband {0}", rubberbandBoost);
+    }
+}
